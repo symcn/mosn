22cf08032a2228c16fc5c39ddc12857016037363
champly <champly@outlook.com>
2020-08-19 14:41:49 +0800

    add http test

--- pkg/upstream/httpregistry/dubbodv2/dubbo_test.go
+++ pkg/upstream/httpregistry/dubbodv2/dubbo_test.go
@@ -1,174 +1,192 @@
 package dubbodv2
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/assert"
+	registry "github.com/symcn/registry/dubbo"
+	dubbocommon "github.com/symcn/registry/dubbo/common"
+	zkreg "github.com/symcn/registry/dubbo/zookeeper"
+	"mosn.io/mosn/pkg/upstream/cluster"
 	_ "mosn.io/mosn/pkg/upstream/cluster"
 )
 
-// func init() {
-// 	Init()
+func init() {
+	Init()
 
-// 	monkey.Patch(zkreg.NewZkRegistry, func(url *dubbocommon.URL) (registry.Registry, error) {
-// 		return &registry.BaseRegistry{}, nil
-// 	})
+	monkey.Patch(zkreg.NewZkRegistry, func(url *dubbocommon.URL) (registry.Registry, error) {
+		return &registry.BaseRegistry{}, nil
+	})
 
-// 	monkey.PatchInstanceMethod(reflect.TypeOf(&registry.BaseRegistry{}), "Register", func(r *registry.BaseRegistry, conf dubbocommon.URL) error {
-// 		return nil
-// 	})
+	monkey.PatchInstanceMethod(reflect.TypeOf(&registry.BaseRegistry{}), "Register", func(r *registry.BaseRegistry, conf *dubbocommon.URL) error {
+		return nil
+	})
 
-// 	monkey.PatchInstanceMethod(reflect.TypeOf(&registry.BaseRegistry{}), "Subscribe", func(r *registry.BaseRegistry, url *dubbocommon.URL, notifyListener registry.NotifyListener) error {
-// 		// do nothing
-// 		return nil
-// 	})
+	monkey.PatchInstanceMethod(reflect.TypeOf(&registry.BaseRegistry{}), "Subscribe", func(r *registry.BaseRegistry, url *dubbocommon.URL, notifyListener registry.NotifyListener) error {
+		// do nothing
+		return nil
+	})
 
-// 	cluster.NewClusterManagerSingleton(nil, nil)
+	monkey.PatchInstanceMethod(reflect.TypeOf(&registry.BaseRegistry{}), "ConnectState", func(r *registry.BaseRegistry) bool {
+		return true
+	})
 
-// }
+	cluster.NewClusterManagerSingleton(nil, nil)
 
-// func TestPubBindFail(t *testing.T) {
-// 	w := httptest.NewRecorder()
+}
 
-// 	r := chi.NewRouter()
-// 	r.Post("/registry/info/sync", registryInfoSync)
+func TestPubBindFail(t *testing.T) {
+	w := httptest.NewRecorder()
 
-// 	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
-// 	req.Header.Set("Content-Type", "application/json")
-// 	r.ServeHTTP(w, req)
-// 	assert.Equal(t, w.Code, 200)
+	r := chi.NewRouter()
+	r.Post("/registry/info/sync", registryInfoSync)
 
-// 	var res ResponseInfo
-// 	err := json.Unmarshal(w.Body.Bytes(), &res)
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, res.Errno, fail)
-// }
+	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
+	req.Header.Set("Content-Type", "application/json")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, 200)
 
-// func TestPub(t *testing.T) {
-// 	w := httptest.NewRecorder()
+	var res ResponseInfo
+	err := json.Unmarshal(w.Body.Bytes(), &res)
+	assert.Nil(t, err)
+	assert.Equal(t, res.Errno, fail)
+}
 
-// 	r := chi.NewRouter()
-// 	r.Post("/registry/info/sync", registryInfoSync)
+func TestPub(t *testing.T) {
+	w := httptest.NewRecorder()
 
-// 	req, _ := http.NewRequest("POST", "/registry/info/sync", strings.NewReader(testRequestStr))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	r.ServeHTTP(w, req)
-// 	assert.Equal(t, w.Code, 200)
+	r := chi.NewRouter()
+	r.Post("/registry/info/sync", registryInfoSync)
 
-// 	var res ResponseInfo
-// 	err := json.Unmarshal(w.Body.Bytes(), &res)
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, res.Errno, succ)
-// }
+	req, _ := http.NewRequest("POST", "/registry/info/sync", strings.NewReader(testRequestStr))
+	req.Header.Set("Content-Type", "application/json")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, 200)
 
-// func TestSubBindFail(t *testing.T) {
-// 	w := httptest.NewRecorder()
+	var res ResponseInfo
+	err := json.Unmarshal(w.Body.Bytes(), &res)
+	assert.Nil(t, err)
+	assert.Equal(t, res.Errno, succ)
+}
 
-// 	r := chi.NewRouter()
-// 	r.Post("/registry/info/sync", registryInfoSync)
+func TestSubBindFail(t *testing.T) {
+	w := httptest.NewRecorder()
 
-// 	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
-// 	req.Header.Set("Content-Type", "application/json")
-// 	r.ServeHTTP(w, req)
-// 	assert.Equal(t, w.Code, 200)
+	r := chi.NewRouter()
+	r.Post("/registry/info/sync", registryInfoSync)
 
-// 	var res ResponseInfo
-// 	err := json.Unmarshal(w.Body.Bytes(), &res)
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, res.Errno, fail)
-// }
+	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
+	req.Header.Set("Content-Type", "application/json")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, 200)
 
-// func TestSub(t *testing.T) {
-// 	w := httptest.NewRecorder()
+	var res ResponseInfo
+	err := json.Unmarshal(w.Body.Bytes(), &res)
+	assert.Nil(t, err)
+	assert.Equal(t, res.Errno, fail)
+}
 
-// 	r := chi.NewRouter()
-// 	r.Post("/registry/info/sync", registryInfoSync)
+func TestSub(t *testing.T) {
+	w := httptest.NewRecorder()
 
-// 	req, _ := http.NewRequest("POST", "/registry/info/sync", strings.NewReader(testRequestStr))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	r.ServeHTTP(w, req)
-// 	assert.Equal(t, w.Code, 200)
+	r := chi.NewRouter()
+	r.Post("/registry/info/sync", registryInfoSync)
 
-// 	var res ResponseInfo
-// 	err := json.Unmarshal(w.Body.Bytes(), &res)
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, res.Errno, succ)
-// }
+	req, _ := http.NewRequest("POST", "/registry/info/sync", strings.NewReader(testRequestStr))
+	req.Header.Set("Content-Type", "application/json")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, 200)
 
-// func TestUnsub(t *testing.T) {
-// 	w := httptest.NewRecorder()
+	var res ResponseInfo
+	err := json.Unmarshal(w.Body.Bytes(), &res)
+	assert.Nil(t, err)
+	assert.Equal(t, res.Errno, succ)
+}
 
-// 	r := chi.NewRouter()
-// 	r.Post("/registry/info/sync", registryInfoSync)
+func TestUnsub(t *testing.T) {
+	w := httptest.NewRecorder()
 
-// 	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
-// 	req.Header.Set("Content-Type", "application/json")
-// 	r.ServeHTTP(w, req)
-// 	assert.Equal(t, w.Code, 200)
-// }
+	r := chi.NewRouter()
+	r.Post("/registry/info/sync", registryInfoSync)
 
-// func TestUnpub(t *testing.T) {
-// 	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
+	req.Header.Set("Content-Type", "application/json")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, 200)
+}
 
-// 	r := chi.NewRouter()
-// 	r.Post("/registry/info/sync", registryInfoSync)
+func TestUnpub(t *testing.T) {
+	w := httptest.NewRecorder()
 
-// 	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
-// 	req.Header.Set("Content-Type", "application/json")
-// 	r.ServeHTTP(w, req)
-// 	assert.Equal(t, w.Code, 200)
-// }
+	r := chi.NewRouter()
+	r.Post("/registry/info/sync", registryInfoSync)
 
-// var testRequestStr = `
-// {
-//     "provider_list": [
-//         {
-//             "service": {
-//                 "interface": "a",
-//                 "methods": [
-//                     "a",
-//                     "b",
-//                     "c"
-//                 ]
-//             },
-//             "host": "1.2.3.4",
-//             "port": 8080
-//         },
-//         {
-//             "service": {
-//                 "interface": "b",
-//                 "methods": [
-//                     "a",
-//                     "b",
-//                     "c"
-//                 ]
-//             },
-//             "host": "1.2.3.4",
-//             "port": 8080
-//         }
-//     ],
-//     "consumer_list": [
-//         {
-//             "service": {
-//                 "interface": "c",
-//                 "methods": [
-//                     "a",
-//                     "b",
-//                     "c"
-//                 ]
-//             },
-//             "host": "1.2.3.4",
-//             "port": 8080
-//         },
-//         {
-//             "service": {
-//                 "interface": "d",
-//                 "methods": [
-//                     "a",
-//                     "b",
-//                     "c"
-//                 ]
-//             },
-//             "host": "1.2.3.4",
-//             "port": 8080
-//         }
-//     ]
-// }
-// `
+	req, _ := http.NewRequest("POST", "/registry/info/sync", nil)
+	req.Header.Set("Content-Type", "application/json")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, w.Code, 200)
+}
+
+var testRequestStr = `
+{
+	"provider_list": [
+		{
+			"service": {
+				"interface": "a",
+				"methods": [
+					"a",
+					"b",
+					"c"
+				]
+			},
+			"host": "1.2.3.4",
+			"port": 8080
+		},
+		{
+			"service": {
+				"interface": "b",
+				"methods": [
+					"a",
+					"b",
+					"c"
+				]
+			},
+			"host": "1.2.3.4",
+			"port": 8080
+		}
+	],
+	"consumer_list": [
+		{
+			"service": {
+				"interface": "c",
+				"methods": [
+					"a",
+					"b",
+					"c"
+				]
+			},
+			"host": "1.2.3.4",
+			"port": 8080
+		},
+		{
+			"service": {
+				"interface": "d",
+				"methods": [
+					"a",
+					"b",
+					"c"
+				]
+			},
+			"host": "1.2.3.4",
+			"port": 8080
+		}
+	]
+}
+`
