a8df48a02d57ba81670dba1429eb28129e246750
champly <champly@outlook.com>
2020-12-15 17:51:33 +0800

    fix eds resource

--- pkg/xds/v3/ldssubscriber.go
+++ pkg/xds/v3/ldssubscriber.go
@@ -4,7 +4,7 @@
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+* the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
 
 package v3
 
@@ -46,7 +46,7 @@ func (c *ADSClient) reqListeners(streamClient envoy_service_discovery_v3.Aggrega
 		},
 	})
 	if err != nil {
-		log.DefaultLogger.Infof("get listener fail: %v", err)
+		log.DefaultLogger.Errorf("get listener fail: %v", err)
 		return err
 	}
 	return nil
